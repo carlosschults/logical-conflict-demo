825bec5e055a96fc5e6ad1a25be991bb6f5532ba
Carlos Schults <carlos.schults@gmail.com>
2021-07-17 11:17:30 -0300

    Implement logic for displaying multiplication tables

--- Program.cs
+++ Program.cs
@@ -6,7 +6,13 @@ namespace logical_conflict_demo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Type the number you want to generate the multiplication table for:");
+            var response = int.Parse(Console.ReadLine());
+
+            for (var i = 1; i <= 10; i++)
+            {
+                Console.WriteLine($"{response} X {i} = {response * i}");
+            }
         }
     }
 }
